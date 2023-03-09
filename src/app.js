8ca3559c79b110f722336428f8a20d9163d319c9
marvelnano <marvelnano@gmail.com>
2023-03-08 19:53:12 -0500

    agregando función logger

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const config = require('./config');
+const logger = require('./loggerMiddleware');
 const routeStudents = require(`./${config.VERSION}/routes/students`);
 const { swaggerDocs: SwaggerDocs } = require(`./${config.VERSION}/swagger/swaggerV2`);
 
@@ -21,12 +22,21 @@ app.use(express.json());
 //var whitelist = ['https://editor.swagger.io/', 'http://example2.com']
 app.use(cors());
 
+app.use(logger);
+
 // * Rutas del app
 app.get('/', (req, res) => {
     res.send('<h2 style="text-align:center">Bienvenido al Api Rest con Node JS</h2>');
 });
 
 app.use(`/api/${config.VERSION}/students`, routeStudents());
+
+app.use((req, res) => {
+    console.log('ruta no identificada: '+req.path);
+    res.status(404).json({
+        error: 'Not found'
+    });
+})
 
 module.exports = {
     param: app,
