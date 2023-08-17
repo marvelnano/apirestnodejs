e95762c0eb5b137d4dd5a3dd5264c8997fddbba5
marvel <marvelnano@gmail.com>
2023-08-17 11:12:16 -0500

    mejorando script de la db para la v3

--- src/v3/database/database.sql
+++ src/v3/database/database.sql
@@ -3,7 +3,7 @@ CREATE TABLE estudiante(
 	name VARCHAR(50) NOT NULL,
 	age INT NOT NULL,
 	enroll VARCHAR(5) NOT NULL
-)
+);
 
 INSERT INTO estudiante (name, age, enroll) VALUES('Antonio', 20, 'true');
 INSERT INTO estudiante (name, age, enroll) VALUES('Mariana', 38, 'false');
@@ -15,7 +15,7 @@ SELECT * FROM estudiante;
 CREATE TABLE usuario(
 	id INT(4) AUTO_INCREMENT PRIMARY KEY,
 	name VARCHAR(50) NOT NULL
-)
+);
 
 INSERT INTO usuario (name) VALUES('jose');
 
